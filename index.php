ae4c74b5013ed3c4054c24057ca388c692affd35
Emilie Coatelant <emiliecoatelant@outlook.fr>
2022-04-21 14:13:08 +0200

    Avancée des cookies et de la session

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ if (!isset($_SESSION['pseudonym']) && !isset($_SESSION['idUtil']) && !isset($_SE
     session_start();
 }
 
+setcookie('utilisateur','emilie', time()+60*60*24*30);
+
 $url = '';
 // recuperer l'url dans un tableau qui prendra les arguments de l'url
 if (isset($_GET['url'])) {
@@ -19,24 +21,24 @@ if (isset($url[0])) {
 }
 
 // Si c'est pas un page
-if (!in_array($page, array('connexion', 'utilisateur', 'technicien', 'admin', 'panier', 'avis', 'contact', 'produits'))) {
-    // Si c'est une page static
-    if (in_array($page, array('home', 'propos', 'mentions', 'articles', 'article1', 'article2', 'article3', 'schoolDev', 'schoolNet', 'E-education', 'test'))) {
-        ob_start();
-        $pageTitle = ucfirst($page) . ' - School APPS';
-        require "static/$page.php";
-        $pageContent = ob_get_clean();
-        require 'layout.php';
-    } else { // Ni module ni page static
-        $error = '404';
-        require "static/error.php";
-        http_response_code(404);
-        die;
-    }
-} else { // Module
-    $pageTitle = ucfirst($page) . ' - School APPS';
-    ob_start();
-    require "modules/$page/mod_$page.php";
-    $pageContent = ob_get_clean();
-    require 'layout.php';
-}
+// if (!in_array($page, array('connexion', 'utilisateur', 'technicien', 'admin', 'panier', 'avis', 'contact', 'produits'))) {
+//     // Si c'est une page static
+//     if (in_array($page, array('home', 'propos', 'mentions', 'articles', 'article1', 'article2', 'article3', 'schoolDev', 'schoolNet', 'E-education', 'test'))) {
+//         ob_start();
+//         $pageTitle = ucfirst($page) . ' - School APPS';
+//         require "static/$page.php";
+//         $pageContent = ob_get_clean();
+//         require 'layout.php';
+//     } else { // Ni module ni page static
+//         $error = '404';
+//         require "static/error.php";
+//         http_response_code(404);
+//         die;
+//     }
+// } else { // Module
+//     $pageTitle = ucfirst($page) . ' - School APPS';
+//     ob_start();
+//     require "modules/$page/mod_$page.php";
+//     $pageContent = ob_get_clean();
+//     require 'layout.php';
+// }
