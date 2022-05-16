466916d41d62419da264939147ae3224f8986f18
ecoatelant <emiliecoatelant@outlook.fr>
2022-05-17 01:26:18 +0200

    Gestion des emails

--- modules/connexion/cont_connexion.php
+++ modules/connexion/cont_connexion.php
@@ -14,7 +14,7 @@
         }
 
         function connexion () {
-            $requete = $this->modeleConnexion->getUtilisateurByEmail($_POST['email']);
+            $requete = $this->modeleConnexion->getUtilisateurParEmail($_POST['email']);
             if ($requete != NULL) {
             // Vérification du mot de passe :
                 if($this->modeleConnexion->verifMDP($_POST['mdp'], $requete[0]['hash_mdp'])){
@@ -42,24 +42,30 @@
         }
 
         function inscription(){
-            //Vérifie si les deux mots de passe rentrées sont identiques
-            if($this->modeleConnexion->mDPRepetesIdentiques($_POST['mdp'], $_POST['mdp_repete'])){
-                //Vérifie si l'utilisateur n'existe pas déjà (le pseudo et l'adresse e-mail doivent être différent)
-                if(!($this->modeleConnexion->verifPseudoEmailUnique($_POST['pseudo'], $_POST['email']))){
-                    //TODO : Le pseudo ou l'email est déjà utilisé
-                    echo '<main>pseudo ou email déjà utilisé</main>';
+            //Vérifie si la page précédente est bien le formulaire d'inscription
+            if(isset($_POST['formulaireInscription'])){
+                //Vérifie si les deux mots de passe rentrées sont identiques
+                if($this->modeleConnexion->mDPRepetesIdentiques($_POST['mdp'], $_POST['mdp_repete'])){
+                    //Vérifie si l'utilisateur n'existe pas déjà (le pseudo et l'adresse e-mail doivent être différent)
+                    if(!($this->modeleConnexion->verifPseudoEmailUnique($_POST['pseudo'], $_POST['email']))){
+                        //TODO : Le pseudo ou l'email est déjà utilisé
+                        echo '<main>pseudo ou email déjà utilisé</main>';
+                    }else{
+                        /*$nouvelUtilisateur =*/ $this->modeleConnexion->nouvelUtilisateur($_POST['pseudo'], $_POST['prenom'], $_POST['nom'], $_POST['email'], $_POST['mdp']);
+                        
+                        //TODO : Vérification de la bonne inscription de l'utilisateur donc attribution des cookies sinon affichage erreur d'inscription
+                        header('Location: '.PATHBASE.'/connexion/formConnexion');
+                        exit;
+                    }
                 }else{
-                    /*$nouvelUtilisateur =*/ $this->modeleConnexion->nouvelUtilisateur($_POST['pseudo'], $_POST['prenom'], $_POST['nom'], $_POST['email'], $_POST['mdp']);
-                    //TODO : Vérification de la bonne inscription de l'utilisateur donc attribution des cookies sinon affichage erreur d'inscription
-                    header('Location: '.PATHBASE.'/connexion/formConnexion');
-                    exit;
+                    //TODO : Les mots de passe ne sont pas identiques.
+                    echo '<main>Les mots de passe ne sont pas identiques.</main>';
                 }
-            }else{
-                //TODO : Les mots de passe ne sont pas identiques.
-                echo '<main>Les mots de passe ne sont pas identiques.</main>';
             }
-            
-            
+            //Si l'utilisateur ne provient pas du formulaire
+            else{
+                //TODO : envoie vers la page 404
+            }
         }
 
         function formConnexion(){
@@ -89,14 +95,56 @@
                         for($i=0; $i<6; $i++){
                             $recup_code.=$caracteres[rand(0, $caracteresLength - 1)];
                         }
+                        //Voir pour peut-être le remplacer par un cookie
                         $_SESSION['recup_code'] = $recup_code;
                         
                         //Vérification si l'utilisateur avait déjà demandé un code de récupération
                         if($this->modeleConnexion->verificationCodeRecuperationInexistant($_SESSION['recup_email'])){
+                            //Si inexistant, création d'un élément
                             $this->modeleConnexion->nouveauCodeRecuperation($_SESSION['recup_email'], $_SESSION['recup_code']);
-                        }else {
+                        }else { //Sinon, mise à jour du code seulement
                             $this->modeleConnexion->mAJCodeRecuperation($_SESSION['recup_email'], $_SESSION['recup_code']);
                         }
+                        $donneesEMail = $this->modeleConnexion->getUtilisateurParEMail($recup_email);
+                        header('Location: '.PATHBASE.'/oops');
+                        // $header="MIME-Version: 1.0\r\n";
+                        // $header.='From:"VEG & CO"<emiliecoatelant2006@mail.com>'."\n";
+                        // $header.='Content-Type:text/html; charset="utf-8"'."\n";
+                        // $header.='Content-Transfer-Encoding: 8bit';
+                        // $message = '
+                        // <html>
+                        // <head>
+                        // <title>Récupération de mot de passe - VEG & CO</title>
+                        // <meta charset="utf-8" />
+                        // </head>
+                        // <body>
+                        // <font color="#303030";>
+                        //     <div align="center">
+                        //     <table width="600px">
+                        //         <tr>
+                        //         <td>
+                                    
+                        //             <div align="center">Bonjour <b>'.$donneesEMail[0]['pseudo'].'</b>,</div>
+                        //             Voici votre code de récupération: <b>'.$recup_code.'</b>
+                        //             Cliquez <a href="http://localhost/vegandco/connexion/recuperation-mdp-oublie?section=code&code='.$recup_code.'">ici</a> pour réinitialiser votre vot mot de passe.
+                        //             A bientôt sur <a href="http://localhost/vegandco/">Votre site</a> !
+                                    
+                        //         </td>
+                        //         </tr>
+                        //         <tr>
+                        //         <td align="center">
+                        //             <font size="2">
+                        //             Ceci est un email automatique, merci de ne pas y répondre
+                        //             </font>
+                        //         </td>
+                        //         </tr>
+                        //     </table>
+                        //     </div>
+                        // </font>
+                        // </body>
+                        // </html>
+                        // ';
+                        // mail($recup_email, "Récupération de mot de passe - VEG AND CO", $message, $header);
                     }else{
                         $_SESSION['erreur'] = "L'adresse e-mail renseignée n'existe pas dans notre base de donnée.";
                         header('Location: '.PATHBASE.'/connexion/mot-de-passe-oublie');
